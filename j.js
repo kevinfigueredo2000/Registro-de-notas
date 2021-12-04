27136e218b57fe9eee1ad78e8e40a3030b36643d
Kevin Figueredo <figueredo.kevin2000@gmail.com>
2021-12-04 09:23:09 -0300

    cambios

--- j.js
+++ j.js
@@ -28,26 +28,28 @@ $("#genprom").on("click",function(){
     let alumno1 = new alumno(nombre, nota1, ndiv, np);
     ndiv.push(parseInt($("#not").val().split(",")));
     $(`#padre`).append(
-                        `<div class="container card contador${contador}" id="prueba">
-                            <div class="row"> 
+                        `<div class="container card contador${contador} mt-3" id="prueba">
+                            <div class="row mt-3" id="apro${contador}"> 
                                 <div class="col-6"> 
-                                    <p>Alumno:${nombre.val()}</p>
-                                    <p>Promedio:${alumno1.calcularPromedio(ndiv)}</p>
+                                    <p>Alumno:<b>${nombre.val()}</b></p>
+                                    <p>Promedio:<b>${alumno1.calcularPromedio(ndiv)}</b></p>
                                 </div>
-                                <div class="col-6" id="p2"> 
-                                    <a href="#" id="eliminar"><i class="fas fa-trash-alt text-right btn btn-danger eliminar"></i></a>
-                                    <a href="#" id="editar"><i class="fas fa-edit btn btn-info"></i></a>
+                                <div class="col-6 mt-3" id="p2"> 
+                                    <a href="#"><i class="fas fa-trash-alt text-right btn btn-danger eliminar${contador}"></i></a>
+                                    <a href="#"><i class="fas fa-edit btn btn-info"></i></a>
                                 </div>
                             </div>
                          </div>`
-                            )     
-    $(`.contador${contador}`).hide(0).fadeIn(1000);                 
+                            )    
+    aprobado();
+    $(`.contador${contador}`).hide(0).fadeIn(1000);     
+    ndiv = [];         
     $("#not").val("");
     $("#nom").val("");
     contador ++;
 })
 $(`.eliminar${contador}`).on("click", function(){
-    $(`.contador${contador}`).remove(0).fadeOut(1000);
+    $(`.contador${contador}`).remove().fadeOut(1000);
 })
 
 $("#mas").on("click", function(){
@@ -55,3 +57,32 @@ $("#mas").on("click", function(){
     $("#not").val("");
     $("#not").focus()
 })
+
+function aprobado(){
+    var alumno1 = new alumno(nombre, nota1, ndiv, np);
+    if(alumno1.calcularPromedio(ndiv) >= 7){
+        $(`#apro${contador}`).append(
+            `
+                <div class="col-sm-12 alert alert-success">
+                    <strong>¡El alumno está aprobado!</strong>
+                </div>
+            `
+        )
+        console.log("aprobado");
+    }else if(alumno1.calcularPromedio(ndiv) <= 7){
+        $(`#apro${contador}`).append(
+            `
+                <div class="col-sm-12 alert alert-warning">
+                    <strong>¡El alumno está desaprobado!</strong>
+                </div>
+            `
+        )
+    }else{
+        $(`#apro${contador}`).append(
+            `
+                <div class="col-sm-12 alert alert-danger">
+                    <strong>¡No se selecciono ninguna nota!</strong>
+                </div>
+            `)
+    }
+}
